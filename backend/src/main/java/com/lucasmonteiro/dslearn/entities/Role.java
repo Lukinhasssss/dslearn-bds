b9980fe7d10a8d86575bfcff6597e9f6c021b677
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-16 13:37:54 -0300

    Offer

--- backend/src/main/java/com/lucasmonteiro/dslearn/entities/Role.java
+++ backend/src/main/java/com/lucasmonteiro/dslearn/entities/Role.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity()
+@Entity
 @Table(name = "tb_role")
 public class Role implements Serializable {
 
--- backend/src/main/java/com/lucasmonteiro/dslearn/entities/User.java
+++ backend/src/main/java/com/lucasmonteiro/dslearn/entities/User.java
@@ -14,7 +14,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-@Entity()
+@Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
 
