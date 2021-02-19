1b6be4412b3901e2898524025efff8d32c5da1dc
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-19 14:13:00 -0300

    Custom content for current user, JPQL query

--- backend/src/main/java/com/lucasmonteiro/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/lucasmonteiro/dslearn/repositories/NotificationRepository.java
@@ -1,11 +1,21 @@
 package com.lucasmonteiro.dslearn.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 
 import com.lucasmonteiro.dslearn.entities.Notification;
+import com.lucasmonteiro.dslearn.entities.User;
 
 @Controller
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	
+	@Query("SELECT obj FROM Notification obj WHERE "
+			+ "(obj.user = :user) AND "
+			+ "(:unreadOnly = false OR obj.read = false) "
+			+ "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 
 }
